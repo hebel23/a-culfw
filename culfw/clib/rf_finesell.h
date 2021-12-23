08ab36c144c045e44c242a7c50367710f4ba7219
hebel23 <hebel23@web.de>
2021-12-23 17:29:35 +0100

    added rf_finesell.c/h

---
+++ culfw/clib/rf_finesell.h
@@ -0,0 +1,11 @@
+// Support for Finesell 868MHz FSK protocol 
+// author: hebel23@web.de
+
+#ifndef _RF_FINESELL_H
+#define _RF_FINESELL_H
+
+/* public prototypes */
+void it_tunein(void);
+void it_func(char *in);
+
+#endif
